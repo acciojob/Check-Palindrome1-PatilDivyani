4c940e7e71fa5d953e301d54d220ac27a6101ce2
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-13 19:14:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	
  let start = 0;
 	let end = str.length - 1;
 	while(start<end){
 		if(str.charAt(start)!=str.charAt(end)){
 			return false;
 		}
+		start++;
+		end--;
 	}
 	return true;
 }
